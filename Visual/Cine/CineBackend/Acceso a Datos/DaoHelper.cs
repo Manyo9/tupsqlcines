10e90a16ba8e1c1acb785778f2dd0be0987fce13
MatiMolina22 <112837@tecnicatura.frc.utn.edu.ar>
2021-11-08 18:39:49 -0300

    Creacion clase DaoHelper y Parametro

---
+++ Visual/Cine/CineBackend/Acceso a Datos/DaoHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineBackend.Acceso_a_Datos
+{
+    class DaoHelper
+    {
+        private string cadenaConexion;
+        private SqlConnection conexion;
+        private SqlCommand comando;
+
+        public DaoHelper()
+        {
+            cadenaConexion = @"";
+            conexion = new SqlConnection(cadenaConexion);
+        }
+
+        public DataTable ConsultarDB(string nombreSP)
+        {
+            DataTable tabla = new DataTable();
+            comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = nombreSP;
+            try
+            {
+                conexion.Open();
+                tabla.Load(comando.ExecuteReader());
+                conexion.Close();
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            return tabla;
+        }
+        public DataTable ConsultarConParametro(string nombreSP, Parametro parametro)
+        {
+            DataTable tabla = new DataTable();
+            comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = nombreSP;
+            comando.Parameters.AddWithValue(parametro.Nombre, parametro.Valor);
+            try
+            {
+                conexion.Open();
+                tabla.Load(comando.ExecuteReader());
+                conexion.Close();
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            return tabla;
+        }
+        public DataTable EjecutarSPSalida(string sp, string nomParametro)
+        {
+            DataTable tabla = new DataTable();
+            comando = new SqlCommand();
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Connection = conexion;
+            comando.CommandText = sp;
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = nomParametro;
+            param.Direction = ParameterDirection.Output;
+            param.SqlDbType = SqlDbType.Int;
+            comando.Parameters.Add(param);
+            try
+            {
+                conexion.Open();
+                comando.ExecuteReader();
+                conexion.Close();
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            return tabla;
+        }
+
+
+    }
+}
---
+++ Visual/Cine/CineBackend/Acceso a Datos/Parametro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineBackend.Acceso_a_Datos
+{
+    class Parametro
+    {
+        public string Nombre { get; set; }
+        public string Valor { get; set; }
+        public Parametro(string nombre, string valor)
+        {
+            Nombre = nombre;
+            Valor = valor;
+        }
+        public Parametro()
+        {
+        }
+    }
+}
