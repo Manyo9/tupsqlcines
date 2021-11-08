8f9c593d22bb46e1e3f96909d5fe6ced36ebd9d8
Abraguas <64881344+Abraguas@users.noreply.github.com>
2021-11-08 18:56:17 -0300

    Crear Daos y Services

---
+++ Visual/Cine/CineBackend/Servicios/IReporteService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineBackend.Servicios
+{
+    interface IReporteService
+    {
+        public DataTable ImprimirTicket(int nroTicket, int idSucursal);
+        public DataTable GetPeliSinVentas(DateTime fechaDesde, DateTime fechaHasta);
+        public DataTable GetPeliMasVistas(DateTime fechaDesde, DateTime fechaHasta);
+        public DataTable GetGananciasMensuales(int anio);
+        public DataTable GetVentasPorSucursal(DateTime fechaDesde, DateTime fechaHasta);
+        public DataTable GetPromVentasPorCajero(int anio);
+    }
+}
---
+++ Visual/Cine/CineBackend/Servicios/ReporteService.cs
@@ -0,0 +1,45 @@
+﻿using CineBackend.AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineBackend.Servicios
+{
+    class ReporteService : IReporteService
+    {
+        private IReporteDao dao;
+
+        public DataTable GetGananciasMensuales(int anio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetPeliMasVistas(DateTime fechaDesde, DateTime fechaHasta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetPeliSinVentas(DateTime fechaDesde, DateTime fechaHasta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetPromVentasPorCajero(int anio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetVentasPorSucursal(DateTime fechaDesde, DateTime fechaHasta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable ImprimirTicket(int nroTicket, int idSucursal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
